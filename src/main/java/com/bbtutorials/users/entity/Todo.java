be4a24e48deec5126d31864337035681d37ca3cf
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-28 12:35:13 +0200

    feat: add start data and entity

---
+++ src/main/java/com/bbtutorials/users/entity/Todo.java
@@ -0,0 +1,29 @@
+package com.bbtutorials.users.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Data
+public class Todo {
+
+    @Id
+    @Column
+    private long id;
+
+    @Column
+    @NotNull(message="{NotNull.Todo.title}")
+    private String title;
+
+    @Column
+    @NotNull(message="{NotNull.Todo.isDone}")
+    private boolean isDone;
+
+    @Column
+    private String description;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,24 @@
+DROP TABLE IF EXISTS todo ;
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
+CREATE TABLE todo (
   id INT PRIMARY KEY,
-  FIRST_NAME VARCHAR(250) NOT NULL,
-  LAST_NAME VARCHAR(250) NOT NULL,
-  EMAIL VARCHAR(250) NOT NULL
+  TITLE VARCHAR(250) NOT NULL,
+  IS_DONE VARCHAR(250) NOT NULL,
+  DESCRIPTION VARCHAR(250)
 );
+
+CREATE TABLE users (
+   id INT PRIMARY KEY,
+   FIRST_NAME VARCHAR(250) NOT NULL,
+   LAST_NAME VARCHAR(250) NOT NULL,
+   EMAIL VARCHAR(250) NOT NULL
+);
+
+INSERT INTO todo (ID, TITLE, IS_DONE, DESCRIPTION) VALUES
+  (1, 'Faire une application Java/Angular', false,'Faire un fork pour la base config'),
+  (2, 'Passer l''entretien technique', false, NULL),
+  (3, 'Faire les courses', true, 'Gruyère, jus de citron');
 
 INSERT INTO users (ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES
   (1, 'first', 'last 1', 'abc1@gmail.com'),
