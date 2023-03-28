52c7e4b8df0942d9641142daae18dc7256dea293
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-28 12:59:56 +0200

    feat: add backend controller

---
+++ src/main/java/com/bbtutorials/users/controller/TodoController.java
@@ -0,0 +1,36 @@
+package com.bbtutorials.users.controller;
+
+import com.bbtutorials.users.entity.Todo;
+import com.bbtutorials.users.entity.Users;
+import com.bbtutorials.users.links.TodoLinks;
+import com.bbtutorials.users.links.UserLinks;
+import com.bbtutorials.users.service.TodoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/")
+public class TodoController {
+
+    @Autowired
+    TodoService todoService;
+
+    @GetMapping(path = TodoLinks.LIST_TODOS)
+    public ResponseEntity<?> listTodos() {
+        log.info("TodoController: list todos");
+        List<Todo> resource = todoService.getAllTodos();
+        return ResponseEntity.ok(resource);
+    }
+
+    @PostMapping(path = TodoLinks.ADD_TODO)
+    public ResponseEntity<?> saveTodo(@RequestBody Todo todo) {
+        log.info("TodoController:  save todo");
+        Todo resource = todoService.saveTodo(todo);
+        return ResponseEntity.ok(resource);
+    }
+}
---
+++ src/main/java/com/bbtutorials/users/links/TodoLinks.java
@@ -0,0 +1,11 @@
+package com.bbtutorials.users.links;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TodoLinks {
+
+    public static final String LIST_TODOS = "/todos";
+    public static final String ADD_TODO = "/todo";
+
+}
