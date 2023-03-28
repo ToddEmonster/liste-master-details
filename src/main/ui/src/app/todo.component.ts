483dd84b2d37f930bccd87345950c6947717515c
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-28 15:25:49 +0200

    feat: back-forth routing main/single-page done

--- src/main/ui/src/app/todo.component.ts
+++ src/main/ui/src/app/todo.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from "@angular/core";
 import {AppService} from "./app.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-todo',
@@ -7,12 +8,22 @@ import {AppService} from "./app.service";
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  todo: any;
+  id: number;
+  todo: any = null;
 
-  constructor(private appService: AppService) {
+  constructor(
+    private appService: AppService,
+    private route: ActivatedRoute,
+    private router: Router) {
+  }
+
+  goBackToMain() {
+    this.router.navigate(["/"]);
   }
 
   ngOnInit() {
+    this.id = +this.route.snapshot.paramMap.get('id');
+    console.log("current id:", this.id);
     console.log("current todo:", this.todo);
   }
 }
