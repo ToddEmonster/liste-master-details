9d13394d9c76ec9307da3b724c15ee9121241e26
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-31 16:43:24 +0200

    fix: fixed repo entity call

--- src/main/java/com/toddemonsters/commitments/repository/CommitmentRepository.java
+++ src/main/java/com/toddemonsters/commitments/repository/CommitmentRepository.java
@@ -1,11 +1,10 @@
 package com.toddemonsters.commitments.repository;
 
 import com.toddemonsters.commitments.entity.Commitment;
-import com.toddemonsters.commitments.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource()
-public interface CommitmentRepository extends JpaRepository<Commitment, Integer>, JpaSpecificationExecutor<Todo>, QuerydslPredicateExecutor<Todo> {}
+public interface CommitmentRepository extends JpaRepository<Commitment, Integer>, JpaSpecificationExecutor<Commitment>, QuerydslPredicateExecutor<Commitment> {}
