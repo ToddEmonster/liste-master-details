296dc8769224618b50ce3b937e54dc56bfee1cef
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-31 16:51:07 +0200

    fix: fix data dates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,7 +11,7 @@ CREATE TABLE commitment (
 );
 
 INSERT INTO commitment (ID, TITLE, START_DATE, END_DATE, ASSOCIATION_NAME, ASSOCIATION_LINK, DESCRIPTION) VALUES
-  (1, 'Responsable Projet Sud', DATE '2016-09-01', DATE '2016-07-01','Ingénieurs Sans Frontières (ISF)', 'https://www.isf-france.org/', 'Suivi des projets Sud'),
-  (2, 'Responsable Communication', DATE '2016-09-01', DATE '2016-07-01','GreenSAT', 'https://greensatagrobiopol.wixsite.com/greensat', 'Création et maintenance du site Wix'),
-  (3, 'Responsable Communication', DATE '2016-09-01', DATE '2016-07-01','Impro''Cibo', 'https://www.facebook.com/improcibo.lapage/', 'Communication des événements du club'),
-  (4, 'Pôle Animation', DATE '2017-09-01', DATE '2017-07-01','K''fet de nuit de l''ENSAT', 'https://www.bde.ensat.fr/clubs/kfetnuit/fiche/', 'Participation aux événements du club');
+  (1, 'Responsable Projet Sud', DATE '2016-09-01', DATE '2017-07-01','Ingénieurs Sans Frontières (ISF)', 'https://www.isf-france.org/', 'Suivi des projets Sud'),
+  (2, 'Responsable Communication', DATE '2016-09-01', DATE '2017-07-01','GreenSAT', 'https://greensatagrobiopol.wixsite.com/greensat', 'Création et maintenance du site Wix'),
+  (3, 'Responsable Communication', DATE '2016-09-01', DATE '2017-07-01','Impro''Cibo', 'https://www.facebook.com/improcibo.lapage/', 'Communication des événements du club'),
+  (4, 'Pôle Animation', DATE '2017-09-01', DATE '2018-07-01','K''fet de nuit de l''ENSAT', 'https://www.bde.ensat.fr/clubs/kfetnuit/fiche/', 'Participation aux événements du club');
