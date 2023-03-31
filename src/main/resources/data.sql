93cd7a6cf0ccdcaeba9deab397529639b5748091
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-31 14:37:27 +0200

    feat: add data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,17 @@
-DROP TABLE IF EXISTS todo ;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS commitments ;
 
-CREATE TABLE todo (
-  id INT PRIMARY KEY,
+CREATE TABLE commitments (
+  ID INT PRIMARY KEY,
   TITLE VARCHAR(250) NOT NULL,
-  IS_DONE VARCHAR(250) NOT NULL,
+  START_DATE DATE NOT NULL,
+  END_DATE DATE,
+  ASSOCIATION_NAME VARCHAR(250) NOT NULL,
+  ASSOCIATION_LINK VARCHAR(250),
   DESCRIPTION VARCHAR(250)
 );
 
-CREATE TABLE users (
-   id INT PRIMARY KEY,
-   FIRST_NAME VARCHAR(250) NOT NULL,
-   LAST_NAME VARCHAR(250) NOT NULL,
-   EMAIL VARCHAR(250) NOT NULL
-);
-
-INSERT INTO todo (ID, TITLE, IS_DONE, DESCRIPTION) VALUES
-  (1, 'Faire une application Java/Angular', false,'Faire un fork pour la base config'),
-  (2, 'Passer l''entretien technique', false, NULL),
-  (3, 'Faire les courses', true, 'Gruyère, jus de citron');
-
-INSERT INTO users (ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES
-  (1, 'first', 'last 1', 'abc1@gmail.com'),
-  (2, 'first', 'last 2', 'abc2@gmail.com'),
-  (3, 'first', 'last 3', 'abc3@gmail.com');
+INSERT INTO commitments (ID, TITLE, START_DATE, END_DATE, ASSOCIATION_NAME, ASSOCIATION_LINK, DESCRIPTION) VALUES
+  (1, 'Responsable Projet Sud', DATE '2016-09-01', DATE '2016-07-01','Ingénieurs Sans Frontières (ISF)', 'https://www.isf-france.org/', 'Suivi des projets Sud'),
+  (2, 'Responsable Communication', DATE '2016-09-01', DATE '2016-07-01','GreenSAT', 'https://greensatagrobiopol.wixsite.com/greensat', 'Création et maintenance du site Wix'),
+  (3, 'Responsable Communication', DATE '2016-09-01', DATE '2016-07-01','Impro''Cibo', 'https://www.facebook.com/improcibo.lapage/', 'Communication des événements du club'),
+  (4, 'Pôle Animation', DATE '2017-09-01', DATE '2017-07-01','K''fet de nuit de l''ENSAT', 'https://www.bde.ensat.fr/clubs/kfetnuit/fiche/', 'Participation aux événements du club');
